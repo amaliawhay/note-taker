fac4c74584644edc1a5f2b2e96c4b982e3a37dea
Amalia Hay <amaliawhay@gmail.com>
2020-07-22 12:21:23 -0700

    html routes created

--- server.js
+++ server.js
@@ -10,9 +10,18 @@ const PORT = 8080;
 //Allows express app to parse data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-//gives express access to HTML and CSS in public dir
+//gives express access to files in public dir
 app.use(express.static("public"));
 
+//returns index file
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+//returns notes file
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
 //server is listening
 app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
