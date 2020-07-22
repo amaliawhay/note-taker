b516821171cebb20b5ec0f97d21298a57ecf7fa6
Amalia Hay <amaliawhay@gmail.com>
2020-07-22 14:28:27 -0700

    get api/notes

--- server.js
+++ server.js
@@ -10,17 +10,30 @@ const PORT = 8080;
 //Allows express app to parse data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-//gives express access to files in public dir
+//Takes /public dir out of URL
 app.use(express.static("public"));
 
-//returns index file
+//Gets index.html
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-//returns notes file
+//Gets notes.html
 app.get("/notes", function (req, res) {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+//api
+app.get("api/notes", function (req, res) {
+  fs.readFile("db.json", function (err, data) {
+    // Check for errors
+    if (err) throw err;
+
+    // Converting to JSON
+    const newNote = JSON.parse(data);
+
+    console.log(newNote); // Prints note
+  });
 });
 //server is listening
 app.listen(PORT, function () {
