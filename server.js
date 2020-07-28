02bdad6d9196f0673e6766d10c480e797827a9ba
Amalia Hay <amaliawhay@gmail.com>
2020-07-27 18:06:09 -0700

    Delete function working

--- server.js
+++ server.js
@@ -51,70 +51,46 @@ app.get("/api/notes", function (req, res) {
 
 //api post
 app.post("/api/notes", function (req, res) {
+  //increments ID
+  idNum++;
+  //holds user input
   const postNote = req.body;
-  // for (i = 0, i < notesDB.length, i++){
-  //   postNote["id"] = i + 1;
-  // };
+  //assign id
+  postNote["id"] = idNum;
+  notesDB.push(postNote);
 
-  fs.readFile("./db/db.json", (err, data) => {
-    if (err) throw err;
-    console.log(data);
-    let file = JSON.parse(data);
-    file.push(postNote);
-
-    fs.writeFile(
-      "./db/db.json",
-      JSON.stringify(file),
-      (err) => {
-        if (err) throw err;
-        console.log("Your note has been added");
-      }
-    );
-  });
-
-  res.redirect("/notes");
+  fs.writeFile(
+    "./db/db.json",
+    JSON.stringify(notesDB),
+    (err) => {
+      if (err) throw err;
+      //console.log("Your note has been added");
+      return res.json(postNote);
+    }
+  );
 });
 
 //api delete
 app.delete("/api/notes/:id", function (req, res) {
-  const db = fs.readFileSync(
-    path.join(__dirname, "./db/db.json")
-  );
-
-  const dbFile = JSON.parse(db);
-
-  let id = req.params.id;
-
-  for (let i = 0; i < dbFile.length; i++) {
-    if (dbFile[i].id.toString() === id) {
-      id.splice(i, 1);
-      console.log(dbFile);
-      break;
+  const selectNote = parseInt(req.params.id);
+  console.log(selectNote);
+  for (i = 0; i < notesDB.length; i++) {
+    if (selectNote === notesDB[i].id) {
+      console.log(notesDB[i]);
+      result = notesDB.splice([i], 1);
+      console.log(result);
+      fs.writeFile(
+        "./db/db.json",
+        JSON.stringify(notesDB),
+        (err) => {
+          if (err) throw err;
+          return res.json(notesDB);
+        }
+      );
     }
   }
-
-  fs.writeFileSync(
-    path.join(__dirname, "./db/db.json"),
-    JSON.stringify(dbFile)
-  );
-
-  res.sendStatus(200);
 });
 
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "public/index.html"));
-});
-/* DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. 
-This means you'll need to find a way to give each note a unique `id` when it's saved.
-In order to delete a note, you'll need to read all notes from the `db.json` file, 
-remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.*/
-//assign id to each posted note
-/*filter? through array of objects in db.json
- */
-
-//remove selected note
-//post all remaining notes to db.json
-//server is listening
 app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
 });
